25adb221ef4272b6d6a4fda89164473d4ada2728
Michael Bleigh <mbleigh@mbleigh.com>
2014-02-26 00:50:08 +0000

    Less ugly doc swap.

--- _plugins/api_json.rb
+++ _plugins/api_json.rb
@@ -2,9 +2,9 @@ require('open-uri')
 require('json')
 require('yaml')
 
-endpoint = open('https://api.divshot.com/documentation/divshot.io.json')
-endpoint = open('http://api.dev.divshot.com:9393/documentation/divshot.io.json')
-$API_JSON = JSON.parse(endpoint.read)
-File.open('_data/http.yml', 'w') do |file|
-  file.write(YAML.dump($API_JSON))
-end
+#endpoint = open('https://api.divshot.com/documentation/divshot.io.json')
+#endpoint = open('http://api.dev.divshot.com:9393/documentation/divshot.io.json')
+#$API_JSON = JSON.parse(endpoint.read)
+#File.open('_data/http.yml', 'w') do |file|
+#  file.write(YAML.dump($API_JSON))
+#end
