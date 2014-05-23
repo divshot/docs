bad326d5450d36eec99f56174d4067d34eca3526
Michael Bleigh <mbleigh@mbleigh.com>
2014-05-23 00:35:58 +0000

    Fix api_json URL

--- _plugins/api_json.rb
+++ _plugins/api_json.rb
@@ -3,8 +3,8 @@ require('openssl')
 require('json')
 require('yaml')
 
-#endpoint = open('https://api.divshot.com/documentation/divshot.io.json', { ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE })
-endpoint = open('http://localhost:3001/documentation/divshot.io.json', { ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE })
+endpoint = open('https://api.divshot.com/documentation/divshot.io.json', { ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE })
+#endpoint = open('http://localhost:3001/documentation/divshot.io.json', { ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE })
 $API_JSON = JSON.parse(endpoint.read)
 File.open('_data/http.yml', 'w') do |file|
   file.write(YAML.dump($API_JSON))
